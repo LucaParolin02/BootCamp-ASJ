73fec7157c2efd9362b42692f77173c18590bc04
lucaParolin <lparolin@petersen.corp>
2023-12-01 13:07:30 -0300

    nueva clase

---
+++ 20231201-Parolin-Clase5/javascript.js
@@ -0,0 +1,38 @@
+//var mensaje="hola mundo";
+//alert(mensaje);
+
+//var mensaje = prompt("Ingrese su texto para saber cantidad de caract");
+//console.log(mensaje.length);
+
+//var edad = prompt ("Ingrese su edad");
+//console.log(parseInt(edad)*365);
+
+/* var num1;
+var num2;
+var resultado;
+num1 = prompt ("asignale un numero a sumar a num1");
+num2 = prompt("asignale un numero a sumar a num2");
+resultado = parseInt(num1) + parseInt(num2)
+console.log(resultado); */
+
+//Ejercicio1
+/* var edad = prompt ("Ingrese su edad");
+edad = parseInt(edad);
+var edadMaxima = prompt ("Ingrese su edad maxima");
+edadMaxima = parseInt(edadMaxima);
+var snackFav = prompt ("Ingrese el nombre de su snack favorito");
+var snackPorDia = prompt ("Ingrese la cantidad de snack que consume por dia");
+var snackDia = parseInt(snackPorDia);
+var snackPrecio = prompt("Ingrese precio de su snack favorito");
+snackPrecio = parseInt(snackPrecio);
+var calcular = ((edadMaxima - edad) * snackDia);
+var resultado= ("Necesitaras " + calcular + " " + snackFav + " para que te alcancen hasta los " + edadMaxima + " años");
+var precio= (calcular * snackPrecio);
+alert(resultado + " ,el precio a gastar en snacks por el resto de tu vida es " + precio); */
+
+//Ejercicio2
+/* var dias = parseInt(prompt("Ingrese la cantidad de días que ira de vacaciones"));
+var presupuestoMaximo = parseInt(prompt("Ingrese el presupuesto maximo que espera gastar"));
+var comida = parseInt(prompt("Ingrese la cantidad de comidas que va a tener en el viaje"));
+var cuantoGastar = (presupuestoMaximo - (comida * dias));
+alert("Podes gastar " + cuantoGastar + " en cada comida para que te alcance la plata durante los " + dias + " días de viaje"); */
