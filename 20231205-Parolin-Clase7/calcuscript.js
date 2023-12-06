a6f7f83a7240b5e8c0e5f3246940ca2b533037f6
lucaParolin <lparolin@petersen.corp>
2023-12-06 10:21:06 -0300

    calculadora casi terminada

--- 20231205-Parolin-Clase7/calcuscript.js
+++ 20231205-Parolin-Clase7/calcuscript.js
@@ -16,57 +16,144 @@ let multiplicar = document.getElementById("multiplicar");
 let borrar = document.getElementById("Borrar");
 let C = document.getElementById("C");
 let CE = document.getElementById("CE");
-
+let operacion = null;
+let valorAnterior = null;
 
 C.addEventListener("click",function(){
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 0;
+        frenteCalculadora.innerText = 0;
 })
+
 CE.addEventListener("click",function(){
     let frenteCalculadora = document.getElementById("frenteCalculadora");
     frenteCalculadora.innerText = 0;
 })
-cero.addEventListener("click",function(){
+cero.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 0;
-})
-uno.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0"){
+        frenteCalculadora.innerText = 0;
+    } else {
+        frenteCalculadora.innerText += 0;
+    }
+});
+uno.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 1;
-})
-dos.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 1;
+    } else {
+        frenteCalculadora.innerText += 1;
+    }
+});
+dos.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 2;
-})
-tres.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 2;
+    } else {
+        frenteCalculadora.innerText += 2;
+    }
+});
+tres.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 3;
-})
-cuatro.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 3;
+    } else {
+        frenteCalculadora.innerText += 3;
+    }
+});
+cuatro.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 4;
-})
-cinco.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 4;
+    } else {
+        frenteCalculadora.innerText += 4;
+    }
+});
+cinco.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 5;
-})
-seis.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 5;
+    } else {
+        frenteCalculadora.innerText += 5;
+    }
+});
+seis.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 6;
-})
-siete.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 6;
+    } else {
+        frenteCalculadora.innerText += 6;
+    }
+});
+siete.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 7;
-})
-ocho.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 7;
+    } else {
+        frenteCalculadora.innerText += 7;
+    }
+});
+ocho.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 8;
-})
-nueve.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 8;
+    } else {
+        frenteCalculadora.innerText += 8;
+    }
+});
+nueve.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText = 9;
-})
-mas.addEventListener("click",function(){
+    if(frenteCalculadora.textContent === "0" || frenteCalculadora.textContent === "+" || frenteCalculadora.textContent === "-" || frenteCalculadora.textContent === "*" || frenteCalculadora.textContent === "/"){
+        frenteCalculadora.innerText = 9;
+    } else {
+        frenteCalculadora.innerText += 9;
+    }
+});
+borrar.addEventListener("click", function() {
     let frenteCalculadora = document.getElementById("frenteCalculadora");
-    frenteCalculadora.innerText += "+";
-})
+    if(frenteCalculadora.textContent.length == 1){
+        frenteCalculadora.innerText = 0;
+    } else {
+        frenteCalculadora.textContent = frenteCalculadora.textContent.slice(0,-1);
+    }
+});
+sumar.addEventListener("click", function() {
+    let frenteCalculadora = document.getElementById("frenteCalculadora");
+    valorAnterior = parseInt(frenteCalculadora.textContent);
+    operacion = "suma";
+    frenteCalculadora.textContent = "+";
+});
+restar.addEventListener("click", function() {
+    let frenteCalculadora = document.getElementById("frenteCalculadora");
+    valorAnterior = parseInt(frenteCalculadora.textContent);
+    operacion = "resta";
+    frenteCalculadora.textContent = "-";
+});
+multiplicar.addEventListener("click", function() {
+    let frenteCalculadora = document.getElementById("frenteCalculadora");
+    valorAnterior = parseInt(frenteCalculadora.textContent);
+    operacion = "multiplicar";
+    frenteCalculadora.textContent = "*";
+});
+dividir.addEventListener("click", function() {
+    let frenteCalculadora = document.getElementById("frenteCalculadora");
+    valorAnterior = parseInt(frenteCalculadora.textContent);
+    operacion = "dividir";
+    frenteCalculadora.textContent = "/";
+});
+
+resultado.addEventListener("click", function() {
+    let frenteCalculadora = document.getElementById("frenteCalculadora");
+    let valorActual = parseInt(frenteCalculadora.textContent);
+    switch (operacion) {
+        case "suma":
+            return frenteCalculadora.innerText = valorAnterior + valorActual;                      
+        case "resta":
+            return frenteCalculadora.textContent = valorAnterior - valorActual;
+        case "dividir":
+            return frenteCalculadora.textContent = valorAnterior / valorActual;
+        case "multiplicar":
+            return frenteCalculadora.textContent = valorAnterior * valorActual;
+        default:
+            break;
+    }
+});
