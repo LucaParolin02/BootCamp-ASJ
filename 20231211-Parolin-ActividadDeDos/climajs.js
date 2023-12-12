204ce2494041916a2468e0d7554129cacc2469ab
lucaParolin <lparolin@petersen.corp>
2023-12-12 09:48:17 -0300

    app clima terminada

--- 20231211-Parolin-ActividadDeDos/climajs.js
+++ 20231211-Parolin-ActividadDeDos/climajs.js
@@ -1,15 +1,18 @@
 let boton = document.getElementById("boton");
 let ciudad = document.getElementById("ciudad");
-let temperatura = document.getElementById("temperatura");
-let wicon = document.getElementById("wicon");
-let descripcion = document.getElementById("descripcion");
 
 function cargarCiudad(){
+    let temperatura = document.getElementById("temperatura");
     let input = document.getElementById("usuarioInput").value;
+    let wicon = document.getElementById("wicon");
+    let descripcion = document.getElementById("descripcion");
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+input+"&appid=fdd533266e28101881f610f2b8f1ebe1")
     .then((response) => response.json()) 
     .then((json) => {
       ciudad.textContent = json.name;
+      temperatura.innerHTML = Math.round(json.main.temp-273,5) + "<sup> °C</sup>";
+      wicon.src = "https://openweathermap.org/img/wn/"+json.weather[0].icon+"@2x.png";
+      descripcion.innerHTML = json.weather[0].description;
     })
     .catch((error) => console.log("Ocurrió un error: " + error));
 }
